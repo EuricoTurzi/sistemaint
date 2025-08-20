a1f84d6b900ec4cdd301280adb561f9457b2999e
Alice Oliveira <aliceoliveira45130@gemail.com>
2025-08-20 15:59:02 -0300

    ticket

--- ticket/forms.py
+++ ticket/forms.py
@@ -4,7 +4,7 @@ from .models import ticketmodel
 class ticketForm(forms.ModelForm):
     class Meta:
         model = ticketmodel
-        fields = ['setor', 'erro', 'correcao', 'devolutiva']  # Incluindo devolutiva
+        fields = ['setor', 'erro', 'correcao']  # Incluindo devolutiva
         widgets = {
             'setor': forms.Select(attrs={'class': 'form-control'}),
             'erro': forms.Textarea(attrs={'class': 'form-control'}),
